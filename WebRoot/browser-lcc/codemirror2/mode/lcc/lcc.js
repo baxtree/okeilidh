eaa7fa7858ab440577d83df8c75b6b5fd7f1d62c
baxtree <baxtree@163.com>
2011-05-30 21:46:32 +0100

    add ABOUT content and improve the syntax highlighter

--- WebRoot/browser-lcc/codemirror2/mode/lcc/lcc.js
+++ WebRoot/browser-lcc/codemirror2/mode/lcc/lcc.js
@@ -10,6 +10,9 @@ CodeMirror.defineMode("lcc", function() {
 					return "lcc-constant";
 				}
 			}
+			else if(ch == ":" && stream.eat(":")){
+				return "lcc-keyword";
+			}
 			else if(ch == "=" && stream.eat(">") || ch == "<" && stream.eat("=")){
 				return "lcc-keyword";
 			}
@@ -22,11 +25,14 @@ CodeMirror.defineMode("lcc", function() {
 			else if(ch == "\|" && stream.eat("\|")){
 				return "lcc-keyword";
 			}
-			else if(/[a-z0-9]/.test(ch)){
+			else if(/[a-z0-9_]/.test(ch)){
 				if(ch == "t" && stream.match("hen") && stream.eol()){
 					return "lcc-keyword";
 				}
 				else if(ch == "o" && stream.eat("r") && stream.eol()){
+					return "lcc-keyword";
+				}
+				else if(ch == "n" && stream.match("ull") && stream.eatSpace() && stream.peek() == "<"){
 					return "lcc-keyword";
 				}
 				else if(ch == "p" && stream.match("lays") && stream.peek() == "("){
@@ -35,12 +41,18 @@ CodeMirror.defineMode("lcc", function() {
 				else if(ch == "k" && stream.match("nows") && stream.peek() == "("){
 					return "lcc-keyword";
 				}
+				else if(ch == "n" && stream.match("ot") && stream.peek() == "("){
+					return "lcc-keyword";
+				}
+				else if(ch == "_" && stream.eatSpace() && (stream.peek() == ")" || stream.peek() == ",")){
+					return "lcc-keyword";
+				}
 				else{
 					stream.eatWhile(/[a-zA-Z0-9_@\.:\/]/);
 					return "lcc-constant";
 				}
 			}
-			else if(/[A-Z_]/.test(ch)){
+			else if(/[A-Z]/.test(ch)){
 				stream.eatWhile(/[a-zA-Z0-9_]/);
 				return "lcc-variable";
 			}
