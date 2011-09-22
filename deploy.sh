abb466cd537a0d78dd0978ced419d743690bfdbd
baxtree <baxtree@163.com>
2011-09-22 04:34:58 +0100

    fix the css

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
-scp WebRoot/index.html s0896253@gateway.inf.ed.ac.uk:/group/cisa/web/OK/drupal/okbuzzer
-#scp -r WebRoot/browser-lcc s0896253@gateway.inf.ed.ac.uk:/group/cisa/web/OK/drupal/okbuzzer
+#scp WebRoot/index.html s0896253@gateway.inf.ed.ac.uk:/group/cisa/web/OK/drupal/okbuzzer
+scp -r WebRoot/browser-lcc s0896253@gateway.inf.ed.ac.uk:/group/cisa/web/OK/drupal/okbuzzer
