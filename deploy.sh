b802d82cdb2533239f4f17dc54bf6c2879f20bf8
baxtree <baxtree@163.com>
2012-10-20 19:18:02 +0100

    add mechanic turk examples

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
-#scp WebRoot/index.html s0896253@ssh.inf.ed.ac.uk:/group/cisa/web/OK/drupal/okeilidh
+scp WebRoot/index.html s0896253@ssh.inf.ed.ac.uk:/group/cisa/web/OK/drupal/okeilidh
 #scp WebRoot/img/okeilidh_on_pi.jpg s0896253@ssh.inf.ed.ac.uk:/group/cisa/web/OK/drupal/okeilidh/img/
-scp -r WebRoot/browser-lcc s0896253@ssh.inf.ed.ac.uk:/group/cisa/web/OK/drupal/okeilidh
+#scp -r WebRoot/browser-lcc s0896253@ssh.inf.ed.ac.uk:/group/cisa/web/OK/drupal/okeilidh
